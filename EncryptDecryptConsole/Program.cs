dd14fd49a419773a4d450da81a34e5324d683a1a
subhamk <subhamk@issquaredinc.com>
2022-09-26 14:44:15 +0530

    Added Input/Output functionality with while loop

--- EncryptDecryptConsole/Program.cs
+++ EncryptDecryptConsole/Program.cs
@@ -10,9 +10,18 @@ namespace EncryptDecryptConsole
     {
         static void Main(string[] args)
         {
-            var input = "Data Source=INHY-DDB-FIT01;Initial Catalog=Conflator;Persist Security Info=True;User Id=sa;Password=@WSX1qaz;MultipleActiveResultSets=True";
-            var output = EncryptDecrypt.Encrypt(input);
-            Console.WriteLine(output);
+            var i = 1;
+            while (i != 0)
+            {
+                //var input = "Data Source=INHY-DDB-FIT01;Initial Catalog=Conflator;Persist Security Info=True;User Id=sa;Password=@WSX1qaz;MultipleActiveResultSets=True";
+                Console.WriteLine("\nInput : ");
+                var input = Console.ReadLine();
+                var output = EncryptDecrypt.Encrypt(input);
+                Console.WriteLine("\nOutput : \n" + output);
+                Console.WriteLine("\nPress 1 to continue and press 0 to exit...");
+                var option = Console.ReadLine();
+                i = Convert.ToInt32(option);
+            }
         }
     }
 }
