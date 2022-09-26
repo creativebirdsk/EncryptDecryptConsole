d71de183e064cdae6f1891ad61840b51e48de0b4
subhamk <subhamk@issquaredinc.com>
2022-09-26 13:11:09 +0530

    Add project files.

---
+++ EncryptDecryptConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecryptConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "Data Source=INHY-DDB-FIT01;Initial Catalog=Conflator;Persist Security Info=True;User Id=sa;Password=@WSX1qaz;MultipleActiveResultSets=True";
+            var output = EncryptDecrypt.Encrypt(input);
+            Console.WriteLine(output);
+        }
+    }
+}
